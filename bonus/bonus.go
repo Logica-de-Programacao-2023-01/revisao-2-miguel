444bfc5d60d7a864006dbe6b0247d2a2b78a44fb
nathangerken7 <126472627+nathangerken7@users.noreply.github.com>
2023-06-24 14:34:03 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -6,6 +6,22 @@ import "errors"
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
 func Destino(caminhos [][2]string) (string, error) {
-	// Seu código aqui
-	return "", errors.New("Not implemented yet")
+	if len(caminhos) == 0 {
+		return "", errors.New("Não foi possível determinar a cidade de destino")
+	}
+
+	destinos := make(map[string]int)
+
+	for _, caminho := range caminhos {
+		destinos[caminho[1]]++
+		destinos[caminho[0]]--
+	}
+
+	for cidade, ocorrencias := range destinos {
+		if ocorrencias > 0 {
+			return cidade, nil
+		}
+	}
+
+	return "", errors.New("Não foi possível determinar a cidade de destino")
 }
